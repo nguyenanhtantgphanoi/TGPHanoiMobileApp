175110da83793ed73b9b72ab440b86504bb21d4f
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-12-19 12:14:43 +0700

    change color image

--- src/utils/renderAoLe.js
+++ src/utils/renderAoLe.js
@@ -1,14 +1,14 @@
 export default function renderAoLe(mauAo) {
     switch (mauAo) {
         case 'white':
-            return require('../../assets/images/trang.jpg');
+            return require('../../assets/images/ao-trang.png');
         case 'red':
-            return require('../../assets/images/do.jpg');
+            return require('../../assets/images/ao-do.png');
         case 'purple':
-            return require('../../assets/images/tim.jpg');
+            return require('../../assets/images/ao-tim.png');
         case 'pink':
-            return require('../../assets/images/hong.jpg');
+            return require('../../assets/images/ao-hong.png');
         default:
-            return require('../../assets/images/xanh.jpg');
+            return require('../../assets/images/ao-trang.png');
     }
 }
